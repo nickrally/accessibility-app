313a2eda0c72a141e301ba5846d0623fb6473e54
nickrally <nickrally2009@gmail.com>
2021-10-12 17:29:57 -0600

    minor change to dialog

--- src/components/Dialog.js
+++ src/components/Dialog.js
@@ -6,6 +6,7 @@ const Dialog = (props) => {
   const dialog = document.getElementById("dialog");
   return createPortal(
     <div id="notification-desc" role="dialog">
+      <h3>Title</h3>
       Information provided by the dialog.
       <button className="x-button" onClick={() => props.close(false)}>
         X
